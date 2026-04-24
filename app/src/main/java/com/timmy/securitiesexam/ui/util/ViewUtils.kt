89f27a1a3d7fa2bc72804127264cea11a0a66a5a
HsiangxMinxHsieh <t19880916@yahoo.com.tw>
2026-04-24 18:42:21 +0800

    1.初步卡片內容效果完成

--- app/src/main/java/com/timmy/securitiesexam/ui/util/ViewUtils.kt
+++ app/src/main/java/com/timmy/securitiesexam/ui/util/ViewUtils.kt
@@ -1,5 +1,9 @@
 package com.timmy.securitiesexam.ui.util
 
+import android.graphics.Color
+import android.widget.TextView
+import androidx.core.graphics.toColorInt
+
 //fun TextView.setSelectStyle() {
 //    isClickable = true
 //    height = getScreenHeightPixels() / 12
@@ -10,6 +14,22 @@ package com.timmy.securitiesexam.ui.util
 //        this.context.getRoundBgById(10, bgColorID = R.color.DarkGray, strokeWidth = 1, strokeColorID = R.color.black),
 //    )
 //}
+
+fun TextView.setTextColorByTarget(current: String?, target: String?) {
+    this.setTextColor(getTextColor(current, target))
+}
+
+fun getTextColor(current: String?, target: String?): Int {
+    val curVal = current?.toDoubleOrNull() ?: return Color.GRAY
+    val tarVal = target?.toDoubleOrNull() ?: return Color.GRAY
+    val diff = curVal - tarVal
+    return when {
+        diff > 0 -> Color.RED   // 漲 -> 紅
+        diff < 0 -> "#4CAF50".toColorInt()
+        else -> Color.GRAY
+    }
+
+}
 
 //fun TextView.setSelectStyleNotBackGround(defaultColor: Int = R.color.red, pressColor: Int = R.color.DarkGray) {
 //    isClickable = true
